4c371879271809a18a5bea6ac2ffae1fab2bdf79
AlejoZarate77 <zaratealejo26@gmail.com>
2023-10-06 11:45:05 -0300

    commit 2

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -2,47 +2,47 @@ import React from "react";
 
 function App() {
   return (
-  <div className="h-screen flex flex-col">
-    {/* Color 1 (Azul) */}
-    <div className="flex-1 bg-gradient-to-tr from-cyan-400 via-blue-200 to-blue-800 to-purple-600"></div>
+    <div className="h-screen flex flex-col">
+      {/* Color 1 (Azul) */}
+      <div className="flex-1 bg-gradient-to-tr from-cyan-400 via-blue-400  to-blue-800 to-purple-600"></div>
 
-    {/* Color 2 (Gris) */}
-    <div className="flex-1 bg-gray-200"></div>
+      {/* Color 2 (Gris) */}
+      <div className="flex-1 bg-gray-200"></div>
 
-    {/* Div Central Superpuesto */}
-    <div className="absolute inset-0 flex flex-col justify-center items-center">
-      {/* Div que engloba título, nav, barra y ambos divs */}
-      <div className="w-11/12 h-4/5 bg-transparent relative mt-[-100px]">
-        {/* Título en el lado izquierdo superior */}
-        <div className="absolute top-4 left-0 bg-transparent">
-          <h1 className="text-4xl text-left bg-transparent">
-            <span style={{ color: "black" }}>LOREM</span>
-            <span style={{ color: "gray" }}>IPSUM</span>
-          </h1>
-        </div>
+      {/* Div Central Superpuesto */}
+      <div className="absolute inset-0 flex flex-col justify-center items-center">
+        {/* Div que engloba título, nav, barra y ambos divs */}
+        <div className="w-11/12 h-4/5 bg-transparent relative mt-[-100px]">
+          {/* Título en el lado izquierdo superior */}
+          <div className="absolute top-4 left-0 bg-transparent">
+            <h1 className="text-4xl text-left bg-transparent">
+              <span style={{ color: "white" }}>LOREM</span>
+              <span style={{ color: "gray" }}>IPSUM</span>
+            </h1>
+          </div>
 
-        {/* Barra de búsqueda a la altura del título y en el lado derecho */}
-        <div className="absolute top-4 right-0 bg-transparent">
-          <input
-            type="text"
-            placeholder="SEARCH"
-            style={{
-              width: "400px",
-              backgroundColor: "transparent",
-              color: "blue",
-            }}
-            className="p-2 pl-8 pr-4 border rounded-lg bg-transparent"
-          />
-        </div>
+          {/* Barra de búsqueda a la altura del título y en el lado derecho */}
+          <div className="absolute top-4 right-0 bg-transparent">
+            <input
+              type="text"
+              placeholder="SEARCH"
+              style={{
+                width: "400px",
+                backgroundColor: "transparent",
+                color: "blue",
+              }}
+              className="p-2 pl-8 pr-4 border rounded-lg bg-transparent"
+            />
+          </div>
 
-        {/* Nav debajo del título y del lado izquierdo */}
-        <div className="absolute top-20 left-0 bg-transparent">
-          <nav>
-            <ul className="flex space-x-4 bg-transparent">
+          {/* Nav debajo del título y del lado izquierdo */}
+          <div className="absolute top-20 left-0 bg-transparent">
+            <nav>
+              <ul className="flex space-x-4 bg-transparent">
                 <li>
                   <a
                     href="#"
-                    className="text-gray-700 hover:text-blue-500 hover:underline bg-transparent"
+                    className="text-gray-50 hover:text-blue-500 hover:underline bg-transparent"
                   >
                     HOME
                   </a>
@@ -50,7 +50,7 @@ function App() {
                 <li>
                   <a
                     href="#"
-                    className="text-gray-700 hover:text-blue-500 hover:underline bg-transparent"
+                    className="text-gray-50 hover:text-blue-500 hover:underline bg-transparent"
                   >
                     SERVICES
                   </a>
@@ -58,7 +58,7 @@ function App() {
                 <li>
                   <a
                     href="#"
-                    className="text-gray-700 hover:text-blue-500 hover:underline bg-transparent"
+                    className="text-gray-50 hover:text-blue-500 hover:underline bg-transparent"
                   >
                     BOOK
                   </a>
@@ -66,7 +66,7 @@ function App() {
                 <li>
                   <a
                     href="#"
-                    className="text-gray-700 hover:text-blue-500 hover:underline bg-transparent"
+                    className="text-gray-50 hover:text-blue-500 hover:underline bg-transparent"
                   >
                     ABOUT US
                   </a>
@@ -74,7 +74,7 @@ function App() {
                 <li>
                   <a
                     href="#"
-                    className="text-gray-700 hover:text-blue-500 hover:underline bg-transparent"
+                    className="text-gray-50 hover:text-blue-500 hover:underline bg-transparent"
                   >
                     CONTACT US
                   </a>
@@ -89,7 +89,7 @@ function App() {
             <div className="w-1/2 h-1/2 bg-gray-50 p-5">
               {/* Contenido del primer div */}
               <div>
-                <h2 className="text-2xl text-left mt-4">MAKE AN APPOINTMENT</h2>
+                <h2 className="text-2xl text-left mt-4 text-stone-500">MAKE AN <p className="text-black font-bold">APPOINTMENT</p></h2>
                 <p className="text-xl">
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the industry's
@@ -148,31 +148,65 @@ function App() {
             </div>
 
             {/* Segundo div debajo del nav */}
-            <div className="w-1/2 h-1/2 bg-gray-50">
+            <div className="w-1/2 h-1/2 bg-gray-50 p-5 formulario-container">
               {/* Contenido del segundo div */}
               <h2 className="text-2xl text-center mt-4">IT'S QUICK & EASY</h2>
-              <div className="flex justify-center items-center h-full bg-gradient-to-tr from-cyan-400 via-blue-200 to-blue-800 to-purple-600">
-                <form>
-                  <h2 className="text-2xl text-">IT'S QUICK & EASY</h2>
-                  <label htmlFor="nombre">Nombre:</label>
-                  <input type="text" id="nombre" name="nombre" />
-                  <br />
+              <div className="flex justify-center items-center h-full bg-gradient-to-tr from-cyan-400 via-blue-400 to-blue-800 to-purple-600">
+                <form className="w-full h-full p-4 flex flex-col">
+                  <h2 className="text-2xl text-center text-white">IT'S QUICK & EASY</h2>
 
-                  <label htmlFor="apellido">Apellido:</label>
-                  <input type="text" id="apellido" name="apellido" />
-                  <br />
+                  {/* Nombre y Apellido en la misma fila */}
+                  <div className="mb-2 flex">
+                    <div className="w-1/2">
+                      <label htmlFor="nombre" className="text-xs">
+                        Nombre:
+                      </label>
+                      <input
+                        type="text"
+                        id="nombre"
+                        name="nombre"
+                        className="border p-2 w-full text-xs"
+                      />
+                    </div>
+                    <div className="w-1/2 ml-2">
+                      <label htmlFor="apellido" className="text-xs">
+                        Apellido:
+                      </label>
+                      <input
+                        type="text"
+                        id="apellido"
+                        name="apellido"
+                        className="border p-2 w-full text-xs"
+                      />
+                    </div>
+                  </div>
 
-                  <label htmlFor="contacto">Contacto:</label>
-                  <input type="text" id="contacto" name="contacto" />
-                  <br />
+                  <div className="mb-2">
+                    <label htmlFor="contacto" className="text-xs">
+                      Contacto:
+                    </label>
+                    <input
+                      type="text"
+                      id="contacto"
+                      name="contacto"
+                      className="border p-2 w-full text-xs"
+                    />
+                  </div>
 
-                  <label htmlFor="mensaje">Mensaje:</label>
-                  <textarea id="mensaje" name="mensaje" />
-                  <br />
+                  <div className="mb-2">
+                    <label htmlFor="mensaje" className="text-xs">
+                      Mensaje:
+                    </label>
+                    <textarea
+                      id="mensaje"
+                      name="mensaje"
+                      className="border p-2 w-full text-xs"
+                    ></textarea>
+                  </div>
 
                   <button
                     type="submit"
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs"
                   >
                     Enviar
                   </button>
