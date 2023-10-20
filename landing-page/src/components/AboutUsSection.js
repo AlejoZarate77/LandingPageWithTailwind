547299999e515f80a3c242c2265d43df6247c17a
AlejoZarate77 <zaratealejo26@gmail.com>
2023-10-20 14:36:34 -0300

    input style and animation

--- landing-page/src/components/AboutUsSection.js
+++ landing-page/src/components/AboutUsSection.js
@@ -1,16 +1,32 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import "../css/AboutUsSection.css";
 
 function AboutUsSection() {
   const sectionStyle = {
-    marginTop: "200px", 
+    marginTop: "200px",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
   };
 
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    // Agregar una animación de entrada cuando la sección es visible
+    setIsVisible(true);
+  }, []);
+
   return (
-    <section style={sectionStyle} className="p-8 flex flex-col justify-center items-center bg-gray-100">
-      <h2 className="text-3xl font-bold mb-4">About Us</h2>
+    <section
+      style={sectionStyle}
+      className={`p-8 flex flex-col justify-center items-center bg-gray-100 ${
+        isVisible ? "animated-section" : ""
+      }`}
+    >
+      <h2 className="text-4xl font-bold mb-6 text-center">
+        Bienvenidos a Nuestra Historia
+      </h2>
       <div className="flex flex-col md:flex-row md:space-x-8">
         <div className="w-full md:w-1/2">
           <img
@@ -20,9 +36,12 @@ function AboutUsSection() {
           />
         </div>
         <div className="w-full md:w-1/2 mt-4 md:mt-0">
-          <p className="text-gray-700 text-lg">
-            Inserta aquí tu descripción sobre tu empresa o proyecto. Puedes
-            contar la historia, la misión, los valores, etc.
+          <p className="text-gray-700 text-lg leading-loose">
+            En nuestra empresa, creemos en la innovación y la pasión. Nuestra
+            historia se teje con la dedicación de un equipo comprometido a
+            ofrecer la mejor calidad y servicio a nuestros clientes. Desde
+            nuestros humildes comienzos hasta nuestra visión actual, hemos
+            mantenido nuestros valores centrales de integridad y excelencia.
           </p>
         </div>
       </div>
