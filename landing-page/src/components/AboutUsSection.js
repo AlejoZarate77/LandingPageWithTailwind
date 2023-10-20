fd26d6f6b01455b5215f361d650d7658d4261cac
AlejoZarate77 <zaratealejo26@gmail.com>
2023-10-20 13:46:59 -0300

    about us
    working on a section so that the client has a little information and description with an image about a product

--- landing-page/src/components/AboutUsSection.js
+++ landing-page/src/components/AboutUsSection.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React from 'react';
 
-function AboutUsSection() {
+function AboutUs() {
   return (
-    <div>
-      <h2>Nuestros Servicios</h2>
-      <div className="productos-servicios">
-        {/* Muestra tus 5 productos de servicios aquí */}
-        <div className="producto">Producto 1</div>
-        <div className="producto">Producto 2</div>
-        <div className="producto">Producto 3</div>
-        <div className="producto">Producto 4</div>
-        <div className="producto">Producto 5</div>
+    <div className="min-h-screen flex items-center justify-center">
+      {/* Ajusta las clases para aumentar el ancho y la altura del contenedor */}
+      <div className="bg-white p-8 rounded-lg shadow-md max-w-2xl mx-auto w-full h-full">
+        <div className="flex flex-col md:flex-row items-center md:items-start">
+          <div className="md:w-1/2 md:pr-4">
+            <img
+              src="tu_imagen_grande.jpg"
+              alt="Imagen de About Us"
+              className="w-full h-auto"
+            />
+          </div>
+          <div className="md:w-1/2 md:pl-4 text-center md:text-left">
+            <h2 className="text-2xl font-bold mb-4">Acerca de Nosotros</h2>
+            <p className="text-gray-700">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+              lacinia justo et justo fringilla, id aliquam dui fermentum. Sed non
+              tristique orci. Nulla facilisi. Cras eu facilisis metus, vel
+              venenatis nulla.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
-export default AboutUsSection;
+export default AboutUs;
