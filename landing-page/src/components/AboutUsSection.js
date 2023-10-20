f7770f162b61083421b637fde60bffa050decf3d
AlejoZarate77 <zaratealejo26@gmail.com>
2023-10-20 14:21:24 -0300

    fixed height

--- landing-page/src/components/AboutUsSection.js
+++ landing-page/src/components/AboutUsSection.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 function AboutUsSection() {
+  const sectionStyle = {
+    marginTop: "200px", 
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  };
+
   return (
-    <section className="h-auto p-8 flex flex-col justify-center items-center bg-gray-100">
+    <section style={sectionStyle} className="p-8 flex flex-col justify-center items-center bg-gray-100">
       <h2 className="text-3xl font-bold mb-4">About Us</h2>
       <div className="flex flex-col md:flex-row md:space-x-8">
         <div className="w-full md:w-1/2">
           <img
-            src="https://hips.hearstapps.com/hmg-prod/images/los-mejores-perfumes-de-oton-o-para-hombre-esquire-1604414321.jpg" // Reemplaza con la ruta de tu imagen
+            src="https://hips.hearstapps.com/hmg-prod/images/los-mejores-perfumes-de-oton-o-para-hombre-esquire-1604414321.jpg"
             alt="About Us"
             className="w-full rounded-lg shadow-lg"
           />
