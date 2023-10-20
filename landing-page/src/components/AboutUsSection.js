800683b775d3b778e139d048a95aef619854c66e
AlejoZarate77 <zaratealejo26@gmail.com>
2023-10-20 15:40:37 -0300

    add a key features section
    -fix the problem that images were not displayed
    -now has 3 descriptions and 3 icons
    -centered below without breaking anything

--- landing-page/src/components/AboutUsSection.js
+++ landing-page/src/components/AboutUsSection.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faCheck, faCog } from "@fortawesome/free-solid-svg-icons"; // Asegúrate de importar los iconos que necesitas
 import "../css/AboutUsSection.css";
 
 function AboutUsSection() {
@@ -11,7 +13,7 @@ function AboutUsSection() {
 
   const images = [
     "https://www.instyle.com/thmb/tKmagi6LkbUratBg4OYt2r60ASA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/022822-spring-perfume-quiz-question-lead-2000-a165ec70588c4a7ca98bb627fa842670.jpg",
-    "https://cdn.thomasnet.com/insights-images/embedded-images/65f03090-bc18-42c8-bd5a-1ab6cea99200/02167a88-06e3-47ad-97d2-6ba10b049713/FullHD/perfume-top-suppliers.jpg",
+    "https://www.clara.es/medio/2023/03/17/perfumes-mas-vendidos_34279b27_230317150602_1200x630.jpg",
     "https://i.ytimg.com/vi/AMlZjs17A-A/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLA30NPD1A_JmQFYlgEBkg2OXJ6dKwg",
   ];
 
@@ -21,7 +23,7 @@ function AboutUsSection() {
   useEffect(() => {
     setIsVisible(true);
 
-    // Cambiar la imagen cada 5 segundos
+    // Cambiar la imagen cada 3 segundos
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, 3000);
@@ -29,7 +31,7 @@ function AboutUsSection() {
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  }, [images.length]);
 
   return (
     <section
@@ -61,6 +63,23 @@ function AboutUsSection() {
             nuestros humildes comienzos hasta nuestra visión actual, hemos
             mantenido nuestros valores centrales de integridad y excelencia.
           </p>
+        </div>
+      </div>
+      
+      {/* Sección de Características */}
+      <div className="mt-8">
+        <h3 className="text-2xl font-semibold mb-4">Características Clave</h3>
+        <div className="flex items-center">
+          <FontAwesomeIcon icon={faStar} className="text-yellow-500 mr-2" />
+          <p>Calidad Superior</p>
+        </div>
+        <div className="flex items-center mt-2">
+          <FontAwesomeIcon icon={faCheck} className="text-green-500 mr-2" />
+          <p>Confiabilidad Garantizada</p>
+        </div>
+        <div className="flex items-center mt-2">
+          <FontAwesomeIcon icon={faCog} className="text-blue-500 mr-2" />
+          <p>Innovación Constante</p>
         </div>
       </div>
     </section>
