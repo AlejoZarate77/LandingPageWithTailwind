a4e53a4e0779692567d98443d5a465b85985dc4a
AlejoZarate77 <zaratealejo26@gmail.com>
2023-10-20 16:26:55 -0300

    create a form with an image in its respective section

--- landing-page/src/components/ContactUsSection.js
+++ landing-page/src/components/ContactUsSection.js
@@ -1,19 +1,73 @@
-import React from "react";
+import React from 'react';
 
-function ContactUsSection() {
+const ContactUsSection = () => {
   return (
-    <div>
-      <h2>Nuestros Servicios</h2>
-      <div className="productos-servicios">
-        {/* Muestra tus 5 productos de servicios aquí */}
-        <div className="producto">Producto 1</div>
-        <div className="producto">Producto 2</div>
-        <div className="producto">Producto 3</div>
-        <div className="producto">Producto 4</div>
-        <div className="producto">Producto 5</div>
+    <div className="flex justify-center items-center h-screen">
+      <div className="border rounded-lg p-4 max-w-xl w-full flex">
+        <form className="flex-1 bg-white shadow-md rounded p-8">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+              Nombre de usuario
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Nombre de usuario"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Contraseña
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Contraseña"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+              Correo electrónico
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Correo electrónico"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="address">
+              Dirección
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="address"
+              type="text"
+              placeholder="Dirección"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Iniciar sesión
+            </button>
+          </div>
+        </form>
+        <div className="flex-1">
+          <img
+            src="https://cdn.shopify.com/s/files/1/0451/1085/3787/files/handsome-man-using-perfume-on-black-background_480x480.jpg?v=1643821746"
+            alt="Imagen"
+            className="w-full rounded-lg"
+          />
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default ContactUsSection;
